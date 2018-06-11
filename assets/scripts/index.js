a77f2d854196c4f8d8b1692c7c874ffd9001169f
luissoldevilla <luis.angelsoto@hotmail.com>
2018-06-11 14:01:30 -0400

    Made table:
    x and O alternate

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,10 +2,24 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const events = require('./events.js')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   // your JS code goes here
+  // $('#1').on('click', events.clickX || events.clickO)
+  // $('#2').on('click', events.clickX || events.clickO)
+  // $('#3').on('click', events.clickX || events.clickO)
+  // $('#4').on('click', events.clickX || events.clickO)
+  // $('#5').on('click', events.clickX || events.clickO)
+  // $('#6').on('click', events.clickX || events.clickO)
+  // $('#7').on('click', events.clickX || events.clickO)
+  // $('#8').on('click', events.clickX || events.clickO)
+  events.addHandlers()
+  $(document).ready(function () {
+    $('#myModal').modal({
+      backdrop: 'static'
+    })
+  })
 })
